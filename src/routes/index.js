bc4379c5fd8fa62232eb216ea3d1a1478a40e45b
TrinhMinhThuan <trinhminhthuan059@gmail.com>
2025-05-26 09:28:42 +0700

    fix contructor response

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
   const result = await handlers.getAllProducts();
-  res.json({data: result});
+  res.json(result).status(200);
 });
 
 export default router;
