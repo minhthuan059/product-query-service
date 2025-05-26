f0a7ec57805668750ed4cc3c7d5279cbee15ecca
TrinhMinhThuan <trinhminhthuan059@gmail.com>
2025-05-26 09:20:12 +0700

    fix cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import express from "express";
 import routes from "./routes/index.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
+
 app.use(express.json());
 app.use("/products", routes);
 app.use(errorHandler);
